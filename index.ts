77c261cf5278aee6a2e3238f88b5582b43e12a81
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-26 13:47:45 +0200

    feat: Create index.ts

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { parse } from "https://deno.land/std@0.84.0/flags/mod.ts";
+import { greet } from "./lib.ts";
+
+const args = parse(Deno.args)
+const name = args.name || 'World'
+
+console.dir(greet(name))
