d442a40300aab72a6977a1190a11178a6cd8e501
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-17 21:32:13 +0200

    refactor: Split into deps files and lock assert version

--- lib_test.ts
+++ lib_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals } from "./test_deps.ts";
 import { greet } from "./lib.ts";
 
 Deno.test("#greet", () => {
